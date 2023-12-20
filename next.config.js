d282579d02ee5df3234c14195750602e8f899b26
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-12-20 20:58:16 +0800

    chore(config): update images domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: [`${process.env.NEXT_PUBLIC_BASE_URL}`],
+		domains: ["127.0.0.1", "glowing-angel-b6da62dfa0.strapiapp.com"],
 	}
 }
 
