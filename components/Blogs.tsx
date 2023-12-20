419331027013bdcd0074987744a3c738a16fc32e
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-12-20 11:13:26 +0800

    chore(components): add initial blog card page for every blog post

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -6,7 +6,11 @@ const Blogs = () => {
 		<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
 			<div>
 				<BlogCard />
+			</div>
+			<div>
 				<BlogCard />
+			</div>
+			<div>
 				<BlogCard />
 			</div>
 		</div>
