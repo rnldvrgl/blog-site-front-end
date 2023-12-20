3bd682d20959fc6836a138c7256b899cf679af8a
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-12-20 21:27:44 +0800

    Update Blogs.tsx

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -13,7 +13,7 @@ const Blogs = ({ blogs }: {
 	})
 
 	return (
-		<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+		<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 '>
 			{filteredBlogs.length > 0 ? filteredBlogs?.map((blog: any) => (
 				<BlogCard key={blog.id} blog={blog} />
 			)) : <h1 className='text-2xl font-semibold text-center'>No blogs found</h1>}
